f20fad7a2479c973bcd990afa7d5250b77c15f6f
lassejep <lasse.jepsen@hotmail.com>
2024-07-08 16:05:53 +0200

    changed wallpapers and attempt to change gtk-theme

---
+++ dot-config/gtk-2.0/gtk.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+gnome_schema="org.gnome.desktop.interface"
+gsettings set "$gnome_schema" icon-theme "catppuccin-mocha-mauve-standard+default"
+gsettings set "$gnome_schema" cursor-theme "Bibata-Modern-Ice"
+gsettings set "$gnome_schema" font-name "Fira Code 10"
+gsettings set "$gnome_schema" color-scheme "prefer-dark"
