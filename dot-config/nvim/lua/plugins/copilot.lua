0bf0cbbf5f2c29de90dea53aea39ec9d4019b2a1
lassejep <lasse.jepsen@hotmail.com>
2024-09-23 04:09:14 +0200

    added copilot chat, and changed copilot to only accept a single line at a time

--- dot-config/nvim/lua/plugins/copilot.lua
+++ dot-config/nvim/lua/plugins/copilot.lua
@@ -4,6 +4,20 @@ return {
 		"github/copilot.vim",
 		opt = {
 			filetypes = { markdown = true },
+			vim.keymap.set("i", "<Tab>", "<Plug>(copilot-accept-line)", { noremap = false }),
 		},
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			debug = true,
+		},
+		vim.keymap.set("n", "<leader>cc", ":CopilotChat<CR>", { noremap = true }),
 	},
 }
