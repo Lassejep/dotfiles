fc6b20c46dd8cb37c3b1cce4d27cd344074c7c4f
lassejep <lasse.jepsen@hotmail.com>
2024-12-12 11:23:34 +0100

    changed back, but with forced window

--- dot-config/hypr/scripts/rofi-radio.sh
+++ dot-config/hypr/scripts/rofi-radio.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-PIPE="/tmp/mpv_audio_pipe"
-
 notification(){
     notify-send "Playing now: " "$@"
 }
@@ -14,37 +12,30 @@ menu(){
 
 main() {
     choice=$(menu | rofi -dmenu | cut -d. -f1)
+
     case $choice in
         1)
             notification "Lofi Girl🎶";
             URL="https://youtu.be/jfKfPfyJRdk"
             ADDITIONAL_ARGS="--volume=60"
+            break
             ;;
         2)
             notification "DR P1";
             URL="https://live-icy.gslb01.dr.dk/A/A03H.mp3"
+            additional_args=""
+            break
             ;;
         3)
             notification "Youtube Music🎶";
             URL="https://music.youtube.com/playlist?list=LM"
             ADDITIONAL_ARGS="--ytdl-raw-options-append=cookies-from-browser=firefox --ytdl-format=bestaudio --shuffle"
+            break
             ;;
-    esac
 
-    [ -p $PIPE ] || mkfifo $PIPE
-    mpv --title="radio-mpv" $URL $ADDITIONAL_ARGS --o=$PIPE --of=nut --ovc=rawvideo --oac=pcm_s16le &
-    mpv --force-window --title="radio-player" $PIPE
-}
+        esac
+        mpv --force-window --title="radio-mpv" $URL $ADDITIONAL_ARGS
+    }
 
-cleanup() {
-    pkill -f radio-mpv
-    pkill -f radio-player
-    rm -f $PIPE 2>/dev/null
-    notify-send "Radio stopped"
-}
-
-if pgrep -f radio-mpv > /dev/null || pgrep -f radio-player > /dev/null; then
-    cleanup
-else
-    main
-fi
+pkill -f radio-mpv || main
+notify-send "Radio stopped"
