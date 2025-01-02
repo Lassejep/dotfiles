4dee059899346ab02d7ee7a105b12720346d2a88
lassejep <lasse.jepsen@hotmail.com>
2025-01-02 08:29:21 +0100

    fix: update cursor theme name to Catppuccin Mocha Light

--- dot-config/hypr/scripts/gtk.sh
+++ dot-config/hypr/scripts/gtk.sh
@@ -2,7 +2,7 @@
 gnome_schema="org.gnome.desktop.interface"
 gsettings set "$gnome_schema" gtk-theme "catppuccin-mocha-mauve-standard+default"
 gsettings set "$gnome_schema" icon-theme "catppuccin-mocha-mauve-standard+default"
-gsettings set "$gnome_schema" cursor-theme "catppuccin-mocha-light-cursors"
+gsettings set "$gnome_schema" cursor-theme "Catppuccin Mocha Light"
 gsettings set "$gnome_schema" cursor-size 24
 gsettings set "$gnome_schema" font-name "Fira Code 10"
 gsettings set "$gnome_schema" color-scheme "prefer-dark"
