18b7ae839e24f782d75ac125c7447a7c259251bc
lassejep <lasse.jepsen@hotmail.com>
2025-03-10 04:46:20 +0100

    switched from copilot to deepseek ai helper

--- dot-config/nvim/lua/plugins/copilot.lua
+++ dot-config/nvim/lua/plugins/copilot.lua
@@ -1,57 +1,23 @@
 return {
 	-- Copilot integration
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		build = ":Copilot auth",
-		event = "InsertEnter",
-		opts = {
-			filetypes = {
-				markdown = true,
-				help = true,
-			},
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				keymap = {
-					accept = false,
-					accept_line = "<tab>",
+	"olimorris/codecompanion.nvim",
+	config = true,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {
+		strategies = {
+			chat = {
+				adapter = "deepseek",
+				keymaps = {
+					close = {
+						modes = { n = "q", i = "q" },
+					},
 				},
+				show_token_count = true,
 			},
 		},
 	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "main",
-		cmd = "CopilotChat",
-		dependencies = {
-			{ "zbirenbaum/copilot.lua" },
-			{ "nvim-lua/plenary.nvim" },
-		},
-		build = "make tiktoken",
-		opts = {
-			chat_autocomplete = false,
-			show_help = true,
-			auto_follow_cursor = false,
-			prompts = {
-				concise = {
-					system_prompt = "Keep you message concise, and with no or minimal code.",
-					description = "Ask the chat to keep their message short. This is not supposed to be use on its own, you have to ask a question and add this prompt with /concise",
-				},
-				Commit = {
-					prompt = "> #git:staged\n\nWrite commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters.",
-				},
-			},
-			mappings = {
-				reset = {
-					normal = "<leader>cl",
-					insert = "",
-				},
-			},
-		},
-		keys = {
-			{ "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "Toggle [C]opilot [C]hat", mode = { "n", "v" } },
-			{ "<leader>cg", "<cmd>CopilotChatCommit<cr>", desc = "[C]ommit [G]it staged", mode = { "n", "v" } },
-		},
-	},
+	keys = { { "<leader>cc", vim.cmd.CodeCompanionChat, desc = "[C]odeCompanion[C]hat" } },
 }
