77985e8fbbb6a07ad06b7e3254ffeb6b371a2822
Lasse Jepsen <lasse.jepsen@hotmail.com>
2025-11-30 10:05:08 +0100

    feat(waybar): removed seldom used keybindings script

--- hypr/scripts/keybindings.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-config_file=~/.config/hypr/conf/keybindings.conf
-keybinds=$(grep -oP '(?<=bind = ).*' $config_file)
-keybinds=$(echo "$keybinds" | sed 's/$mainMod/SUPER/g'|  sed 's/,\([^,]*\)$/ = \1/' | sed 's/, exec//g' | sed 's/^,//g')
-
-rofi -dmenu -replace -p "Keybinds" -config ~/.config/rofi/config-compact.rasi <<< "$keybinds"
