032a061025564246c2b6b73e554fa09f2d94f0f4
Lasse Jepsen <lasse.jepsen@hotmail.com>
2026-05-14 15:34:50 +0200

    fix(hyprland): I don't know why but nvidia gpu settings work best for intel gpu as well

--- hypr/conf/env.lua
+++ hypr/conf/env.lua
@@ -3,11 +3,10 @@ hl.env("XCURSOR_THEME", "Catppuccin Mocha Light")
 hl.env("GTK2_RC_FILES", "$XDG_CONFIG_HOME/gtk-2.0/")
 hl.env("QT_QPA_PLATFORM", "wayland")
 hl.env("GRIMBLAST_EDITOR", "swappy -f")
-
---[[ Nvidia fixes
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("XDG_SESSION_TYPE", "wayland")
+
+-- Nvidia GPU fixes
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("GBM_BACKEND", "nvidia-drm")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 hl.env("WLR_DRM_NO_ATOMIC", "1")
---]]
